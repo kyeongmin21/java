1dd307fd64ea94288a7506f39cb8cdf6e01fe1c3
민 <66239952+kyeongmin21@users.noreply.github.com>
2023-03-10 16:58:03 +0900

    Update Arrays.java

--- src/main/java/project/Arrays.java
+++ src/main/java/project/Arrays.java
@@ -7,5 +7,10 @@ public class Arrays {
         for (int i = 0; i < arr.length; i++) {
             System.out.println(arr[i]);
         }
+        
+         // for(타입 변수명) : 콜렉션명
+        for (String data: arr) {
+            System.out.println(data);
+        }
     }
 }
