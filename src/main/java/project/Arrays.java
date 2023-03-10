dbd12eee3b0e5b04a640428133715c577a7d7af9
kyeongmin1011 <kyeongmin@safety-as.com>
2023-03-10 16:44:40 +0900

    java 배열

---
+++ src/main/java/project/Arrays.java
@@ -0,0 +1,11 @@
+package project;
+
+public class Arrays {
+    public static void main(String[] args) {
+        String[] arr = {"one", "two", "three"};
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+}
